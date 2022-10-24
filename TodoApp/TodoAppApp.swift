56e9436a7194da4a9a992a38efa56f60ecf45c9a
DuckiTucki <87948949+DuckiTucki@users.noreply.github.com>
2022-10-25 01:11:43 +0200

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Victor Svensson on 2022-10-25.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
