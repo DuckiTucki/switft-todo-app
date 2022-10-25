02947fb93b02efd60e4609da344f07886c67f496
DuckiTucki <87948949+DuckiTucki@users.noreply.github.com>
2022-10-25 02:14:08 +0200

    Category list designing and displaying of data

--- TodoApp/ContentView.swift
+++ TodoApp/ContentView.swift
@@ -7,19 +7,27 @@
 
 import SwiftUI
 
+struct todoList: Hashable {
+    let title: String
+    let cateogory: String
+    var isDone = false
+}
+
 struct ContentView: View {
-    var categoryList = ["All" ,"Shopping list", "Work", "Activites"]
+    let selectedCategory = 0;
+    let categoryList = ["All", "Shopping", "Work", "Activites", "School"]
     var body: some View {
         NavigationStack{
             ScrollView (.horizontal, showsIndicators: false) {
-                HStack (spacing: 20) {
-                    Text("Hej")
-                        .padding([.bottom, .top], 10)
-                        .padding([.horizontal], 20)
-                        .background(.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(50)
-                    Text("Hejsan")
+                HStack (spacing: 10) {
+                    ForEach(categoryList.indices) {i in
+                        Text(categoryList[i])
+                            .padding([.bottom, .top], 10)
+                            .padding([.horizontal], 20)
+                            .background(selectedCategory == i ? .blue : Color(UIColor.lightGray))
+                            .foregroundColor(.white)
+                            .cornerRadius(50)
+                    }
                 }
                 .padding()
             }
